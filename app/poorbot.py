32717f5def0fd03040abf51d0669f637ff60214d
Mino <madness48596@gmail.com>
2020-04-15 00:53:18 +0800

    Use pyppeteer2, Due to pyppeteer has no maintainer

--- app/poorbot.py
+++ app/poorbot.py
@@ -56,6 +56,9 @@ class PoorBot(RRBot):
         return await self.do_work()
 
     async def idle(self):
+        await self.refresh()
+        await self.check_login()
+
         # work cooldown time
         work_c = 600
 
